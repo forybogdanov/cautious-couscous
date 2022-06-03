8af074639639da2d4292dafe04f8f7a7fbadd686
forybogdanov <nikiforn04@gmail.com>
2022-06-04 01:34:23 +0300

    Add comments

--- Source1.cpp
+++ Source1.cpp
@@ -38,6 +38,9 @@ Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS)
 LiquidCrystal_I2C lcd(0x20, 16, 2);
 Servo motor;
 
+/// <summary>
+/// initial settings of the device
+/// </summary>
 void setup()
 {
 	motor.attach(10);
@@ -53,20 +56,36 @@ void setup()
 	delay(2000);
 }
 
+/// <summary>
+/// clears the screen and displays a new message by a given string
+/// </summary>
+/// <param name="message"></param>
 void displayMessage(String message) {
 	lcd.clear();
 	lcd.setCursor(0, 0);
 	lcd.print(message);
 }
 
+/// <summary>
+/// returns true or false based on whether a profile is selected
+/// </summary>
+/// <returns></returns>
 bool isProfileSelected() {
 	return profileId != NOT_SELECTED_PROFILE;
 }
 
+/// <summary>
+/// returns true or false based on whether a mode is selected
+/// </summary>
+/// <returns></returns>
 bool isModeSelected() {
 	return modeSelected != "";
 }
 
+/// <summary>
+/// sets the profile id and prints messages for the two possible mods
+/// </summary>
+/// <param name="id"></param>
 void selectProfile(int id) {
 	if (isProfileSelected)
 	{
@@ -79,6 +98,9 @@ void selectProfile(int id) {
 	}
 }
 
+/// <summary>
+/// resets all variables to initial values
+/// </summary>
 void resetLock() {
 	pos = 0;
 	profileId = NOT_SELECTED_PROFILE;
@@ -94,10 +116,16 @@ void resetLock() {
 	changePassword = false;
 }
 
+/// <summary>
+/// endless function - the heart of the app
+/// </summary>
 void loop()
 {
 	int correct = 0;
 	int i;
+	/// <summary>
+	/// reads a key from the keypad
+	/// </summary>
 	char customKey = customKeypad.getKey();
 	if (customKey)
 	{
@@ -116,7 +144,10 @@ void loop()
 			selectProfile(3);
 			break;
 		case '*':
-			if (isProfileSelected())
+			/// <summary>
+			/// if the user has selected a profile and has not selected a mode it selects change pass mode
+			/// </summary>
+			if (isProfileSelected() && !isModeSelected())
 			{
 				modeSelected = CHANGE_PASSWORD_MODE;
 				changePassword = true;
@@ -126,79 +157,94 @@ void loop()
 			}
 			break;
 		case '#':
-			if (isProfileSelected())
+			/// <summary>
+			/// if there is no user we don't go furhter
+			/// </summary>
+			if (!isProfileSelected()) 
 			{
-				if (!isModeSelected())
+				break;
+			}
+			/// <summary>
+			/// if there is no mode selected we select unlock door mode
+			/// </summary>
+			if (!isModeSelected())
+			{
+				modeSelected = UNLOCK_DOOR_MODE;
+				lcd.clear();
+				lcd.setCursor(0, 0);
+				lcd.print("ENTER PASSWORD:");
+				break;
+			}
+			/// <summary>
+			/// counts the correct numbers
+			/// </summary>
+			for (i = 0; i < 6; i++)
+			{
+				if (inputCode[i] == passwords[profileId][i])
 				{
-					modeSelected = UNLOCK_DOOR_MODE;
-					lcd.clear();
-					lcd.setCursor(0, 0);
-					lcd.print("ENTER PASSWORD:");
-					break;
+					correct++;
 				}
-				for (i = 0; i < 6; i++)
+			}
+			/// <summary>
+			/// if all numbers are correct it checks if the mode is change pass mode or unlock door
+			/// if it is change pass mode it asks the user to enter the new one
+			/// if it is unlock door mode it unlocks it
+			/// </summary>
+			if (correct == 6)
+			{
+				lcd.clear();
+				lcd.setCursor(0, 0);
+				lcd.print("Input correct!");
+				delay(1000);
+				if (changePassword)
 				{
-					if (inputCode[i] == passwords[profileId][i])
-					{
-						correct++;
-					}
-				}
-				if (correct == 6)
-				{
-					lcd.clear();
-					lcd.setCursor(0, 0);
-					lcd.print("Input correct!");
 					delay(1000);
-					if (changePassword)
+					lcd.clear();
+					lcd.setCursor(0, 0);
+					lcd.print("NEW PASSWORD:");
+					i = 0;
+					while (i < 6)
 					{
-						delay(1000);
-						lcd.clear();
-						lcd.setCursor(0, 0);
-						lcd.print("NEW PASSWORD:");
-						i = 0;
-						while (i < 6)
+						char secretKey = customKeypad.getKey();
+						if (secretKey)
 						{
-							char secretKey = customKeypad.getKey();
-							if (secretKey)
-							{
-								passwords[profileId][i] = secretKey;
-								lcd.setCursor(i, 1);
-								lcd.print(passwords[profileId][i]);
-								i++;
-							}
+							passwords[profileId][i] = secretKey;
+							lcd.setCursor(i, 1);
+							lcd.print(passwords[profileId][i]);
+							i++;
 						}
-						changePassword = false;
-						displayMessage("Successful!");
-						delay(2000);
 					}
-					else
-					{
-						lcd.setCursor(0, 0);
-						lcd.print(" Please Come In ");
-						digitalWrite(relayPin, HIGH);
-						motor.write(90);
-						delay(2000);
-						lcd.clear();
-						lcd.setCursor(0, 0);
-						lcd.print("TIME OUT!");
-						motor.write(0);
-						delay(600);
-						digitalWrite(relayPin, LOW);
-						delay(2000);
-					}
+					changePassword = false;
+					displayMessage("Successful!");
+					delay(2000);
 				}
 				else
 				{
+					lcd.setCursor(0, 0);
+					lcd.print(" Please Come In ");
+					digitalWrite(relayPin, HIGH);
+					motor.write(90);
+					delay(2000);
 					lcd.clear();
 					lcd.setCursor(0, 0);
-					lcd.print("Wrong password");
-					lcd.setCursor(0, 1);
-					lcd.print(" Try Again ");
+					lcd.print("TIME OUT!");
+					motor.write(0);
+					delay(600);
 					digitalWrite(relayPin, LOW);
 					delay(2000);
 				}
-				resetLock();
 			}
+			else
+			{
+				lcd.clear();
+				lcd.setCursor(0, 0);
+				lcd.print("Wrong password");
+				lcd.setCursor(0, 1);
+				lcd.print(" Try Again ");
+				digitalWrite(relayPin, LOW);
+				delay(2000);
+			}
+			resetLock();
 			break;
 		default:
 			if (isModeSelected())
